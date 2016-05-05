1aaed96bd3565d606c1b315a634899c958fc37bc
Liam Ma <liam.q.ma@gmail.com>
2016-05-05 22:30:48 +1000

    Fix lint

--- common/container/app.js
+++ common/container/app.js
@@ -1,9 +1,9 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Items from '../component/items';
 import styles from './app.css';
 
-export default function App(props) {
+function App(props) {
   return (
     <div className={styles.app}>
       <Items {...props} />
